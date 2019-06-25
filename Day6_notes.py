fd91b622f9815d678497bbccdeccf282cdff080c
Fayaz-94 <52184008+Fayaz-94@users.noreply.github.com>
2019-06-25 07:16:16 +0530

    Add files via upload

---
+++ Day6_notes.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#welcome All....to a new week of Python training!!!
+
+
+# In[ ]:
+
+
+# entire week recap - topics
+
+#installation of python & Anaconda.
+
+# introdcution of strings- completed
+
+# introductions of list -- in progress
+
+
+# In[2]:
+
+
+#Slicing of list:
+
+players =['kohli','dhoni','yuvaraj','sachin','vijay shankar']
+
+print(players[2:5])
+
+
+# In[4]:
+
+
+print(players[:2])
+
+
+# In[5]:
+
+
+print('captions of indian team are below')
+for player in players[:2]:
+    print(player.title())
+
+
+# In[6]:
+
+
+#copying a list
+
+my_food = ['pizza','pastry','pasta','chicken']
+
+friends_food =my_food[:]
+
+#friends_food = my_food
+
+
+# In[7]:
+
+
+print(friends_food)
+
+
+# In[10]:
+
+
+my_food.append('choclate','x','y')
+
+friends_food.append('ice cream')
+
+print(my_food)
+print(friends_food)
+
+
+# In[ ]:
+
+
+#tuple
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
